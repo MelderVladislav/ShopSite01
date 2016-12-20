0772b4e3689e9eaf999984c028a95d505bbc25cd
MelderVladislav <melder.vladislav@gmail.com>
2016-12-21 00:27:59 +0300

    добавлена меню, товары, картинки, все базовое

--- ShopSite/Controllers/ShopController.cs
+++ ShopSite/Controllers/ShopController.cs
@@ -8,10 +8,23 @@ namespace ShopSite.Controllers
 {
     public class ShopController : Controller
     {
-        // GET: Shop
-        public ActionResult Index()
+        public ActionResult HomePage()
         {
             return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Categories()
+        {
+            u0263406_shopBaseEntities context = new u0263406_shopBaseEntities();
+            var categoriesList = context.CATEGORY.ToList();
+            return View(categoriesList);
+        }
+        public ActionResult Products(int CategoryId)
+        {
+            u0263406_shopBaseEntities context = new u0263406_shopBaseEntities();
+            var productsList = context.PRODUCT.Where(c => c.CATEGORY_ID == CategoryId).ToList();
+            return View(productsList);
         }
     }
 }
