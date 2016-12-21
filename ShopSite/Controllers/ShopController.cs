c96e5058b0f3370cbb7c0f8f2fd9ea1fcfe97e78
MelderVladislav <melder.vladislav@gmail.com>
2016-12-21 04:38:29 +0300

    Добавлена корзина

--- ShopSite/Controllers/ShopController.cs
+++ ShopSite/Controllers/ShopController.cs
@@ -26,5 +26,32 @@ namespace ShopSite.Controllers
             var productsList = context.PRODUCT.Where(c => c.CATEGORY_ID == CategoryId).ToList();
             return View(productsList);
         }
+       
+        public ActionResult Basket()
+        {
+            string MyCookie = Request.Cookies["order"].Value;
+            string[] masProducts = MyCookie.Split('&');
+            u0263406_shopBaseEntities context = new u0263406_shopBaseEntities();
+            int tempId;
+            List<PRODUCT> purchaseList=new List<PRODUCT>();
+            PRODUCT tempProduct;
+            bool tempConvert;
+            if (masProducts.Length!=0)
+            foreach (string str in masProducts)
+            {
+              
+                    tempConvert = Int32.TryParse(str.ToString(), out tempId);
+                    if (tempConvert)
+                    {
+                        tempProduct = context.PRODUCT.Where(c => c.CATEGORY_ID == tempId).ToList().FirstOrDefault();
+                        if (tempProduct != null)
+                        {
+                            purchaseList.Add(tempProduct);
+                        }
+                    }
+
+            }
+            return View(purchaseList);
+        }
     }
 }
